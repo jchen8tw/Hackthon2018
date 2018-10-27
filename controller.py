f731e5c971492e8688078ca9653a176b9883f2af
Kenchu123 <k88913n@gmail.com>
2018-10-28 00:21:12 +0800

    u

--- controller.py
+++ controller.py
@@ -1,23 +1,3 @@
-stage = 0 # change when longpress
-stage1_index = 0
-stage2_index = 0
-correctFront, correctLeft, correctBack, correctRight = [], [], [], []
-kinds = ['餐廳', '建築', '商店', '公車站'] # 0: stop, 1: inKind, 2: option(front, left, back, right)
-
-names = []
-names.append('Front')
-for i in range(correctFront):
-    names.append(i['name'])
-names.append('Right')
-for i in range(correctRight):
-    names.append(i['name'])
-names.append('Left')
-for i in range(correctLeft):
-    names.append(i['name'])
-names.append('Back')
-for i in range(correctBack):
-    names.append(i['name'])
-    
 def speak(inp):
     print(inp, end = ', ')
 
