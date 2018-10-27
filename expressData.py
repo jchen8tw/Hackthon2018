be0431fc4ee27a3c514fa760e908ad3529792152
Chris Huang <chris100253@gmail.com>
2018-10-27 23:34:09 +0800

    update

--- expressData.py
+++ expressData.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
-def checkPos(degree, latNlng, des_lat, dest_lng, viewPointDegree):
+def checkPos(_degree, latNlng, des_lat, dest_lng, viewPointDegree):
+	degree = _degree * math.pi / 180
 	unit_vector = np.array([math.cos(degree), math.sin(degree)])
 	des_vector = np.array([des_lat - latNlng[0], dest_lng - latNlng[1]])
 	check = np.dot(unit_vector, des_vector)
@@ -10,6 +11,22 @@ def checkPos(degree, latNlng, des_lat, dest_lng, viewPointDegree):
 		return True
 	else:
 		return False
+def findByDirection(degree, latNlng, datas):
+	correctFront = []
+	correctLeft = []
+	correctBack = []
+	correctRight = []
+	for data in datas:
+		if checkPos(degree, latNlng, data['lat'], data['lng'], 90):
+			correctFront.append(data)
+		elif checkPos((degree + 270) % 360, latNlng, data['lat'], data['lng'], 90):
+			correctLeft.append(data)
+		elif checkPos((degree + 90) % 360, latNlng, data['lat'], data['lng'], 90):
+			correctRight.append(data)
+		elif checkPos((degree + 180) % 360, latNlng, data['lat'], data['lng'], 90):
+			correctBack.append(data)
+	return (correctFront, correctLeft, correctBack, correctRight)
+
 def expressData(direction, degree, datas, latNlng, correctData, viewPointDegree):
 	correctDegree = 0
 	if direction == 'front':
