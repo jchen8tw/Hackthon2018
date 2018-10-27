0dc915df175af6641d314828e0911e1dee483d24
Kenchu123 <k88913n@gmail.com>
2018-10-28 05:36:51 +0800

    up

--- googleMap.py
+++ googleMap.py
@@ -13,97 +13,98 @@ GPIO.setup(channel_2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 flag = 0
 
 if len(sys.argv) == 2:
-	googleMapKey = sys.argv[1]
+    googleMapKey = sys.argv[1]
 elif len(sys.argv) == 1:
-	googleMapKey = "AIzaSyCq6_Zryl4bgEIk_f8NDv7A3-niGGfuKNM"
+    googleMapKey = "AIzaSyCq6_Zryl4bgEIk_f8NDv7A3-niGGfuKNM"
 else:
-	exit(-1)
+    exit(-1)
 gmaps = googlemaps.Client(key = googleMapKey)
 Exceptions = googlemaps.exceptions
 
 def getData(lat, lng, key):
-	try:
-		loc = {'lat': lat, 'lng': lng}
-		query_result = gmaps.places_nearby(keyword = key,location = loc, radius = 1000)
-	except (Exceptions.ApiError, Exceptions.TransportError, Exceptions.HTTPError, Exceptions.Timeout):
-		print("ERROR: getData")
-	else:
-		return query_result
+    try:
+        loc = {'lat': lat, 'lng': lng}
+        query_result = gmaps.places_nearby(keyword = key,location = loc, radius = 1000)
+    except (Exceptions.ApiError, Exceptions.TransportError, Exceptions.HTTPError, Exceptions.Timeout):
+        print("ERROR: getData")
+    else:
+        return query_result
 
-def functionshutdown():
-	flag = 1
+def functionshutdown(channel):
+    if GPIO.input(channel):
+        flag = 1
 
 
 if __name__ == '__main__':
-	stage = 0 # change when longpress
-	stage1_index = 0
-	stage2_index = 0
-	kinds = ['Restaurant', 'Building', 'Store', 'Bus stop'] # 0: stop, 1: inKind, 2: option(front, left, back, right)
-	
-	GPIO.add_event_detect(channel_2, GPIO.FALLING, callback = functionshutdown)
-	input_key = -1
-	while True:
-		if flag == 1:
-			flag = 0
-			stage, stage1_index, stage2_index = doublePress(stage, stage1_index, stage2_index)
-			continue
-		print('start wait for edge')
-		GPIO.wait_for_edge(channel_1, GPIO.FALLING)
-		init_time = time.time()
-		GPIO.wait_for_edge(channel_1, GPIO.RISING)
-		current_time = time.time()
-		if init_time - current_time >= 2:
-			input_key = 0
-			print('process start')
-		else:
-			input_key = 1
-		#input_key = int(input('input press: '))
-		if input_key == 0:
-			if stage == 0:
-				stage, stage1_index = longPress(stage, stage1_index)
-				latNlng = [24.7871229, 120.9967369]
-				degree = 0
-				speak(kinds[0])
-			elif stage == 1:
-				stage, stage2_index = longPress(stage, stage2_index)
-				speak('loading')
-				results = getData(latNlng[0], latNlng[1], kinds[stage1_index])
-				datas = []
-				for result in results['results']:
-					datas.append({
-						'lat': result['geometry']['location']['lat'],
-						'lng': result['geometry']['location']['lng'],
-						'name': result['name'],
-						'place_id': result['place_id']
-					})
-				correctFront, correctLeft, correctBack, correctRight = findByDirection(degree, latNlng, datas)
-				names = []
-				names.append('Front')
-				if len(correctFront):
-					for i in correctFront:
-						names.append(i['name'])
-				names.append('Right')
-				if len(correctRight):
-					for i in correctRight:
-						names.append(i['name'])
-				names.append('Left')
-				if len(correctLeft):
-					for i in correctLeft:
-						names.append(i['name'])
-				names.append('Back')
-				if len(correctBack):
-					for i in correctBack:
-						names.append(i['name'])
-				speak('finish')
-				speak(names[0])
+    stage = 0 # change when longpress
+    stage1_index = 0
+    stage2_index = 0
+    kinds = ['Restaurant', 'Building', 'Store', 'Bus stop'] # 0: stop, 1: inKind, 2: option(front, left, back, right)
+    
+    GPIO.add_event_detect(channel_2, GPIO.FALLING, callback = functionshutdown)
+    input_key = -1
+    while True:
+        if flag == 1:
+            flag = 0
+            stage, stage1_index, stage2_index = doublePress(stage, stage1_index, stage2_index)
+            continue
+        print('start wait for edge')
+        GPIO.wait_for_edge(channel_1, GPIO.FALLING)
+        init_time = time.time()
+        GPIO.wait_for_edge(channel_1, GPIO.RISING)
+        current_time = time.time()
+        if current_time - init_time >= 2:
+            input_key = 0
+            print('process start')
+        else:
+            input_key = 1
+        #input_key = int(input('input press: '))
+        if input_key == 0:
+            if stage == 0:
+                stage, stage1_index = longPress(stage, stage1_index)
+                latNlng = [24.7871229, 120.9967369]
+                degree = 0
+                speak(kinds[0])
+            elif stage == 1:
+                stage, stage2_index = longPress(stage, stage2_index)
+                speak('loading')
+                results = getData(latNlng[0], latNlng[1], kinds[stage1_index])
+                datas = []
+                for result in results['results']:
+                    datas.append({
+                        'lat': result['geometry']['location']['lat'],
+                        'lng': result['geometry']['location']['lng'],
+                        'name': result['name'],
+                        'place_id': result['place_id']
+                    })
+                correctFront, correctLeft, correctBack, correctRight = findByDirection(degree, latNlng, datas)
+                names = []
+                names.append('Front')
+                if len(correctFront):
+                    for i in correctFront:
+                        names.append(i['name'])
+                names.append('Right')
+                if len(correctRight):
+                    for i in correctRight:
+                        names.append(i['name'])
+                names.append('Left')
+                if len(correctLeft):
+                    for i in correctLeft:
+                        names.append(i['name'])
+                names.append('Back')
+                if len(correctBack):
+                    for i in correctBack:
+                        names.append(i['name'])
+                speak('finish')
+                speak(names[0])
 
-		elif input_key == 1:
-			if stage == 1:
-				stage, stage1_index = changeOptions(stage, stage1_index, kinds)
-			elif stage == 2:
-				stage, stage2_index = changeOptions(stage, stage2_index, names)
-		elif input_key == 2:
-			stage, stage1_index, stage2_index = doublePress(stage, stage1_index, stage2_index)
+        elif input_key == 1:
+            if stage == 1:
+                stage, stage1_index = changeOptions(stage, stage1_index, kinds)
+            elif stage == 2:
+                stage, stage2_index = changeOptions(stage, stage2_index, names)
+        elif input_key == 2:
+            stage, stage1_index, stage2_index = doublePress(stage, stage1_index, stage2_index)
 
 
 
