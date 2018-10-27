14fda3ed66147bf87bc15339f38227f681c0c545
Chris Huang <chris100253@gmail.com>
2018-10-27 21:15:55 +0800

    update

--- expressData.py
+++ expressData.py
@@ -1,25 +1,27 @@
 import math
 import numpy as np
-def checkPos(degree, latNlng, des_lat, dest_lng):
+def checkPos(degree, latNlng, des_lat, dest_lng, viewPointDegree):
 	unit_vector = np.array([math.cos(degree), math.sin(degree)])
 	des_vector = np.array([des_lat - latNlng[0], dest_lng - latNlng[1]])
 	check = np.dot(unit_vector, des_vector)
-	if check >= 0:
+	cosine = check / (np.sqrt(np.square(unit_vector).sum()) * np.sqrt(np.square(des_vector).sum()))
+	des_degree = math.acos(cosine) / (2 * math.pi) * 360
+	if des_degree <= viewPointDegree / 2:
 		return True
 	else:
 		return False
-def expressData(direction, datas, latNlng, lat, lng):
+def expressData(direction, degree, datas, latNlng, correctData, viewPointDegree):
+	correctDegree = 0
 	if direction == 'front':
-		degree = degree
+		correctDegree = degree
 	elif direction == 'left':
-		degree = (degree + 270) % 360
+		correctDegree = (degree + 270) % 360
 	elif direction == 'right':
-		degree = (degree + 90) % 360
+		correctDegree = (degree + 90) % 360
 	elif direction == 'back':
-		degree = (degree + 180) % 360
-	correctData = []
+		correctDegree = (degree + 180) % 360
 	for data in datas:
-		if checkPos(degree, latNlng, data['lat'], data['lng']):
+		if checkPos(correctDegree, latNlng, data['lat'], data['lng'], viewPointDegree):
 			correctData.append(data)
 	if len(correctData):
 		return True
