c1ac2eac0ec411710ef144d748adbe495273d934
Chris Huang <chris100253@gmail.com>
2018-10-28 12:40:22 +0800

    u

--- controller.py
+++ controller.py
@@ -27,20 +27,34 @@ def doublePress(stage, index1, index2):
 	speak('exit')
 	return stage, index1, index2
 
-def leadMe(degree, latNlng, lat, lng):
-    toDirectVector = np.array([lng - latNlng[1], lat - latNlng[0]])
-    orientVector = np.array([math.cos((90 - degree) * math.pi / 180), math.sin((90 - degree) * math.pi / 180)])
-    cosTheta = np.dot(orientVector, toDirectVector) / np.sqrt(np.square(orientVector).sum()) / np.sqrt(np.square(toDirectVector).sum())
-    cross = np.cross(orientVector, toDirectVector)
-    if cosTheta < 0:
-        if cosTheta < -1:
-            cosTheta = -0.99999
-        if cross < 0:
-            speak("Turn Left for degree " + str(int(math.acos(cosTheta)*180/math.pi)) + " and walk straight")
-        else:
-            speak("Turn Right for degree " + str(int(math.acos(cosTheta))*180/math.pi) + " and walk straight")
-    else:
-        speak("It is in back of you")
+def leadMe(_degree, latNlng, des_lat, des_lng):
+    # toDirectVector = np.array([lng - latNlng[1], lat - latNlng[0]])
+    # orientVector = np.array([math.cos((90 - degree) * math.pi / 180), math.sin((90 - degree) * math.pi / 180)])
+    # cosTheta = np.dot(orientVector, toDirectVector) / np.sqrt(np.square(orientVector).sum()) / np.sqrt(np.square(toDirectVector).sum())
+    # cross = np.cross(orientVector, toDirectVector)
+    degree = _degree * math.pi / 180
+	unit_vector = np.array([math.cos(degree), math.sin(degree)])
+	des_vector = np.array([des_lat - latNlng[0], dest_lng - latNlng[1]])
+	check = np.dot(unit_vector, des_vector)
+	cosine = check / (np.sqrt(np.square(unit_vector).sum()) * np.sqrt(np.square(des_vector).sum()))
+	des_degree = math.acos(cosine) / (2 * math.pi) * 
+	cross = np.cross(unit_vector, des_vector)
+	if des_degree <= 90:
+		if cross > 0:
+			speak("Turn Left for degree " + str(int(des_degree)) + " and walk straight")
+		else:
+			speak("Turn Right for degree " + str(int(des_degree) + " and walk straight")
+	else:
+		speak("It is in back of you")
+    # if cosTheta < 0:
+    #     if cosTheta < -1:
+    #         cosTheta = -0.99999
+    #     if cross < 0:
+    #         speak("Turn Left for degree " + str(int(math.acos(cosTheta)*180/math.pi)) + " and walk straight")
+    #     else:
+    #         speak("Turn Right for degree " + str(int(math.acos(cosTheta))*180/math.pi) + " and walk straight")
+    # else:
+    #     speak("It is in back of you")
 
 
     
