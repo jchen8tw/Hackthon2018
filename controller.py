4b0493bcbbf7267507468049f7691fe540f3a7a7
Chris Huang <chris100253@gmail.com>
2018-10-28 13:45:53 +0800

    u

--- controller.py
+++ controller.py
@@ -34,16 +34,16 @@ def leadMe(_degree, latNlng, des_lat, des_lng):
     # cross = np.cross(orientVector, toDirectVector)
     degree = _degree * math.pi / 180
     unit_vector = np.array([math.cos(degree), math.sin(degree)])
-    des_vector = np.array([des_lat - latNlng[0], dest_lng - latNlng[1]])
+    des_vector = np.array([des_lat - latNlng[0], des_lng - latNlng[1]])
     check = np.dot(unit_vector, des_vector)
     cosine = check / (np.sqrt(np.square(unit_vector).sum()) * np.sqrt(np.square(des_vector).sum()))
-    des_degree = math.acos(cosine) / (2 * math.pi) * 
+    des_degree = math.acos(cosine) / (2 * math.pi) * 360
     cross = np.cross(unit_vector, des_vector)
     if des_degree <= 90:
         if cross > 0:
             speak("Turn Left for degree " + str(int(des_degree)) + " and walk straight")
         else:
-            speak("Turn Right for degree " + str(int(des_degree) + " and walk straight")
+            speak("Turn Right for degree " + str(int(des_degree)) + " and walk straight")
     else:
         speak("It is in back of you")
     # if cosTheta < 0:
